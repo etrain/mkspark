38c44922119110c22600f5fc596a3bc5afb0309c
Evan Sparks <sparks@cs.berkeley.edu>
2014-11-09 18:19:19 -0800

    80g was a little unreasonable.

--- skeleton/run-spark.sh
+++ skeleton/run-spark.sh
@@ -10,7 +10,7 @@ $SPARK_HOME/bin/spark-submit \
   --master `cat /root/spark-ec2/cluster-url` \
   --deploy-mode client \
   --class $CLASS \
-  --driver-memory 80423M \
+  --driver-memory 4g \
   --driver-java-options "-Dspark.local.dir=/mnt/spark,/mnt1/spark -XX:+UseG1GC -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
   target/scala-2.10/$PROJECT-assembly-0.1.jar \
   "$@"
